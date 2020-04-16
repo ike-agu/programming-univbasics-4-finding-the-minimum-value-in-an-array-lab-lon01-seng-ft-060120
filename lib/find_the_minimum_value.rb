8e89b69523c84bf188c47589dbba5dfe69afe97a
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-16 20:59:45 +0000

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,16 @@
+
+
+
 def find_min_value(array)
-  # Add your solution here
-end
+count = 0 
+lowest_value = 50
+while count < array.length do 
+  if lowest_value >= array[count]
+lowest_value = array[count]
+  end 
+  count +=1
+end 
+lowest_value
+
+
+end 
