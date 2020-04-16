92746e68d37a513aba8d62bd56f83d761643a68a
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-16 20:55:02 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,16 @@
+
+require "pry"
+
 def find_min_value(array)
-  # Add your solution here
-end
+count = 0 
+lowest_value = 1 
+while count < array.length do 
+  if lowest_value >= array[count]
+lowest_value = array[count]
+  end 
+  count +=1
+end 
+lowest_value
+
+binding.pry
+end 
